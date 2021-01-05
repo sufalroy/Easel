3df2e8c1aa9fc4445a270e718ca0d4321701b6ad
sufal <sufal@sufalroy1997gmail.com>
2021-01-05 10:06:44 +0530

    Added OpenGL debugger

--- Easel/src/Platform/OpenGL/GLContext.cpp
+++ Easel/src/Platform/OpenGL/GLContext.cpp
@@ -2,7 +2,7 @@
 #include "GLContext.h"
 
 #include "GL.h"
-
+#include "GLDebug.h"
 
 #include <GLFW/glfw3.h>
 
@@ -14,6 +14,117 @@
 #endif // EASEL_PLATFORM_WINDOWS
 
 #include <imgui.h>
+
+#ifdef GL_DEBUG_CALLBACK
+
+static std::string GetStringForType(GLenum type) {
+
+	switch (type) {
+
+	case GL_DEBUG_TYPE_ERROR:
+		return"Error";
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+		return "Deprecated behavior";
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+		return "Undefined behavior";
+	case GL_DEBUG_TYPE_PORTABILITY:
+		return "Portability issue";
+	case GL_DEBUG_TYPE_PERFORMANCE:
+		return "Performance issue";
+	case GL_DEBUG_TYPE_MARKER:
+		return "Stream annotation";
+	case GL_DEBUG_TYPE_OTHER_ARB:
+		return "Other";
+	default:
+		return "";
+	}
+}
+
+static bool PrintMessage(GLenum type)
+{
+	switch (type) {
+
+	case GL_DEBUG_TYPE_ERROR:
+		return true;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+		return true;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+		return true;
+	case GL_DEBUG_TYPE_PORTABILITY:
+		return true;
+	case GL_DEBUG_TYPE_PERFORMANCE:
+		return false;
+	case GL_DEBUG_TYPE_MARKER:
+		return false;
+	case GL_DEBUG_TYPE_OTHER_ARB:
+		return false;
+	default:
+		return false;
+	}
+}
+
+static std::string GetStringForSource(GLenum source) {
+
+	switch (source) {
+
+	case GL_DEBUG_SOURCE_API:
+		return "API";
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+		return "Window System";
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:
+		return "Shader compiler";
+	case GL_DEBUG_SOURCE_THIRD_PARTY:
+		return "Third party";
+	case GL_DEBUG_SOURCE_APPLICATION:
+		return "Application";
+	case GL_DEBUG_SOURCE_OTHER:
+		return "Other";
+	default:
+		return "";
+	}
+}
+
+namespace Easel {
+
+	static std::string GetStringForSeverity(GLenum severity) {
+		
+		switch (severity) {
+
+		case GL_DEBUG_SEVERITY_HIGH:
+			EASEL_ASSERT(0, "");
+			return "High";
+		case GL_DEBUG_SEVERITY_MEDIUM:
+			EASEL_ASSERT(0, "");
+			return "Medium";
+		case GL_DEBUG_SEVERITY_LOW:
+			EASEL_ASSERT(0, "");
+			return "Low";
+		case GL_DEBUG_SEVERITY_NOTIFICATION:
+			return "Notification";
+		case GL_DEBUG_SOURCE_API:
+			return "Source API";
+		default:
+			return("");
+		}
+	}
+
+	void APIENTRY openglCallbackFunction(GLenum source, GLenum type, GLuint id,
+		GLenum severity, GLsizei length,
+		const GLchar* message, const void* userParam) {
+
+		if (!PrintMessage(type))
+			return;
+
+		EASEL_CORE_INFO(OPENGLLOG"Message: {0}", message);
+		EASEL_CORE_INFO(OPENGLLOG"Type: {0}", GetStringForType(type));
+		EASEL_CORE_INFO(OPENGLLOG"Source: {0}", GetStringForSource(source));
+		EASEL_CORE_INFO(OPENGLLOG"ID: {0}", id);
+		EASEL_CORE_INFO(OPENGLLOG"Severity: {0}", GetStringForSeverity(source));
+	}
+}
+
+#endif // GL_DEBUG_CALLBACK
+
 
 namespace Easel {
 
@@ -28,8 +139,8 @@ namespace Easel {
 			HGLRC tempContext = wglCreateContext(hDc);
 			wglMakeCurrent(hDc, tempContext);
 
-			if (!wglMakeCurrent(hDc, tempContext))
-			{
+			if (!wglMakeCurrent(hDc, tempContext)) {
+
 				EASEL_CORE_ERROR("Failed to initialize OpenGL context");
 			}
 
@@ -52,19 +163,19 @@ namespace Easel {
 			};
 
 			HGLRC hrc = wglCreateContextAttribsARB(hDc, nullptr, contextAttribsList);
-			if (hrc == nullptr)
-			{
+			if (hrc == nullptr) {
+
 				EASEL_CORE_ERROR("Failed to create core OpenGL context");
 			}
-			else
-			{
+			else {
+
 				wglMakeCurrent(NULL, NULL);
 				wglDeleteContext(tempContext);
 				wglMakeCurrent(hDc, hrc);
 			}
 #else
-			if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-			{
+			if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+
 				EASEL_CORE_ERROR("Failed to initialize OpenGL context");
 			}
 #endif
@@ -72,10 +183,23 @@ namespace Easel {
 			glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
 			EASEL_CORE_INFO("----------------------------------");
+			EASEL_CORE_INFO(OPENGLLOG);
 			EASEL_CORE_INFO(glGetString(GL_VERSION));
 			EASEL_CORE_INFO(glGetString(GL_VENDOR));
 			EASEL_CORE_INFO(glGetString(GL_RENDERER));
 			EASEL_CORE_INFO("----------------------------------");
+
+#if EASEL_DEBUG
+#ifdef GL_DEBUG_CALLBACK
+			EASEL_CORE_INFO(OPENGLLOG"Registering OpenGL debug callback");
+
+			glEnable(GL_DEBUG_OUTPUT);
+			glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+			glDebugMessageCallback(openglCallbackFunction, nullptr);
+			GLuint unusedIds = 0;
+			glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, &unusedIds, true);
+#endif
+#endif
 		}
 
 		GLContext::~GLContext() = default;
---
+++ Easel/src/Platform/OpenGL/GLDebug.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "GL.h"
+
+namespace Easel {
+
+#ifdef EASEL_DEBUG
+	#ifdef glDebugMessageCallback
+		#define GL_DEBUG_CALLBACK 1
+	#else
+		#define GL_DEBUG 1
+	#endif
+#endif // EASEL_DEBUG
+
+#define OPENGLLOG "[OPENGL] - "
+
+#ifdef GL_DEBUG
+	bool GLLogCall(const char* function, const char* file, const int32_t line);
+	void GLClearError();
+#endif // GL_DEBUG
+
+}
+
+#ifdef GL_DEBUG
+#define GLCall(x) GLClearError();\
+		x;\
+		if(!GLLogCall(#x, __FILE__, __LINE__)) EASEL_BREAK();
+#else
+#define GLCall(x) x
+#endif // GL_DEBUG
