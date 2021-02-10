c52160074a046896d5da6328e72b42bf96c15b9c
sufal <sufal@sufalroy1997gmail.com>
2021-02-10 23:00:33 +0530

    Adding OpenGL FrameBuffer

---
+++ Easel/src/Platform/OpenGL/GLFramebuffer.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "Easel/Graphics/API/Framebuffer.h"
+#include "GL.h"
+#include "GLTexture.h"
+
+#include <glm/glm.hpp>
+
+namespace Easel {
+
+	namespace Graphics {
+
+		enum class Format;
+
+		class EASEL_EXPORT GLFramebuffer : public Framebuffer {
+		public:
+			GLFramebuffer();
+			GLFramebuffer(const FramebufferInfo& bufferInfo);
+			~GLFramebuffer();
+
+			inline uint32_t GetFramebuffer() const { return m_Handle; }
+
+			void GenerateFramebuffer() override;
+
+			void Bind(uint32_t width, uint32_t height) const override;
+			void Bind() const override;
+			void UnBind() const override;
+			void Clear() override {}
+			uint32_t GetWidth() const override { return m_Width; }
+			uint32_t GetHeight() const override { return m_Height; }
+
+			GLenum GetAttachmentPoint(TextureFormat format);
+
+			inline void SetClearColour(const glm::vec4& colour) override {
+				m_ClearColour = colour;
+			}
+			void AddTextureAttachment(TextureFormat format, Texture* texture) override;
+			void AddCubeTextureAttachment(TextureFormat format, CubeFace face, TextureCube* texture) override;
+
+			void AddShadowAttachment(Texture* texture) override;
+			void AddTextureLayer(int index, Texture* texture) override;
+
+			void Validate() override;
+
+			static void MakeDefault();
+		protected:
+			static Framebuffer* CreateFuncGL(const FramebufferInfo& bufferInfo);
+		private:
+			uint32_t m_Handle;
+			uint32_t m_Width, m_Height, m_ColourAttachmentCount;
+			glm::vec4 m_ClearColour;
+			std::vector<GLenum> m_AttachmentData;
+			bool m_ScreenFramebuffer = false;
+		};
+	}
+}
